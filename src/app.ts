b755661886d43dde4785da3103405bfffd8e61e8
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-29 00:41:51 +0900

    Feat: DB 연결 및 서버 실행 로직 수정

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import express from 'express';
-import { env } from './config/envconfig';
-import { router } from './routes';
-
-const port = Number(env.PORT || 3000);
+import env from './config/envconfig';
+import db from './config/dbconfig';
+import router from './routes';
 
 const app = express();
+const port = Number(env.PORT || 3000);
 
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+db.getConnection()
+  .then(async () => {
+    console.log('✅ AWS MySQL DB 접속 성공');
+
+    app.listen(port, () => {
+      console.log('DB_HOST:', env.DB_HOST);
+      console.log('DB_NAME:', env.DB_NAME);
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log('⛔ DB 접속 및 서버 실행 실패', error));
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
