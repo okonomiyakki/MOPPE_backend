882a3f556426db4d733275bf2a07b86bba2520fe
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-29 00:40:49 +0900

    Fix: DB_NAME 필드명 수정

--- src/config/envconfig.ts
+++ src/config/envconfig.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const env = {
+const env = {
   BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS,
 
   PORT: process.env.PORT,
 
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
-  DB_DBNAME: process.env.DB_NAME,
+  DB_NAME: process.env.DB_NAME,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
 
@@ -19,3 +19,5 @@ export const env = {
   REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
   REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
 };
+
+export default env;
