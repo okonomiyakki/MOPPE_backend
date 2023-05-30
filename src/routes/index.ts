4f037d6d685304b801546e2428705a2ed3f12816
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-30 23:53:27 +0900

    Feat: 회원 가입 라우터 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import rootRouter from './rootRouter';
+import userRouter from './userRouter';
 
 const router = Router();
 
 router.use('/', rootRouter);
+router.use('/users', userRouter);
 
 export default router;
