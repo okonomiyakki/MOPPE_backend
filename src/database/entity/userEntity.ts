0ad4c8346eb992991fe6ba150e1873e5e648c76e
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-30 13:42:22 +0900

    Feat: 유저 회원가입, 로그인, 정보수정 input 타입 생성

--- src/database/entity/userEntity.ts
+++ src/database/entity/userEntity.ts
@@ -1,5 +1,28 @@
 import { Entity, Column, PrimaryGeneratedColumn, Timestamp, OneToMany } from 'typeorm';
 
+interface UserProfile {
+  user_id: number;
+  user_email: string;
+  user_name: string;
+  user_password: string;
+  user_career_goal: string;
+  user_stacks: string;
+  user_introduction: string;
+  user_img: string;
+  user_created_at: Timestamp;
+}
+
+export type signUpUserInput = Pick<UserProfile, 'user_email' | 'user_name' | 'user_password'>;
+
+export type logInUserInput = Pick<UserProfile, 'user_email' | 'user_password'>;
+
+export type updatUserInput = Partial<
+  Pick<
+    UserProfile,
+    'user_name' | 'user_career_goal' | 'user_stacks' | 'user_introduction' | 'user_img'
+  >
+>;
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
