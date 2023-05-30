f3c43fd9a7637e1f742bb217841979da23e15aaa
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-31 05:46:08 +0900

    Update: 루트페이지 수정

--- src/routes/rootRouter.ts
+++ src/routes/rootRouter.ts
@@ -3,7 +3,9 @@ import { Router, Request, Response } from 'express';
 const rootRouter = Router();
 
 rootRouter.get('/', (req: Request, res: Response) => {
-  res.send(`This is __mogakppo__ root page`);
+  res.send(
+    `<h1 style="width: 100%; background: lightgray; margin: 0; padding: 0; text-align: center;">모각뽀 루트 페이지입니다 ㅎㅎ</h1>`
+  );
 });
 
 export default rootRouter;
