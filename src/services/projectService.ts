6d4b3db79395e8906a48734830eba99458e2397f
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-01 07:01:16 +0900

    Feat: 모집 역할별 모집글 목록 조회 서비스 로직 선언 (1차)

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -20,3 +20,22 @@ export const addProject = async (inputData: CreateProjectInput) => {
     }
   }
 };
+
+/* 모집 역할별 모집글 목록 조회 */
+export const getProjectsByRole = async (project_role: string) => {
+  try {
+    const foundProjectsByRole = await projectRepo.findProjectByRole(project_role);
+
+    // 게시글이 존재하는지 확인 후 없으면 에러 처리
+
+    return foundProjectsByRole;
+  } catch (error) {
+    if (error instanceof AppError) {
+      if (error.statusCode === 500) console.log(error);
+      throw error;
+    } else {
+      console.log(error);
+      throw new AppError(500, '[ 서버 에러 ] 모집 역할별 모집글 목록 실패');
+    }
+  }
+};
