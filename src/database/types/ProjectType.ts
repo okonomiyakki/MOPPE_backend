a8e706412728c98d971db2370bb50fe238658613
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-31 18:34:14 +0900

    Fix: 프로젝트 인터페이스 널 타입 추가

--- src/database/types/ProjectType.ts
+++ src/database/types/ProjectType.ts
@@ -6,10 +6,10 @@ interface ProjectProfile {
   project_title: string;
   project_summary: string;
   project_recruitment_role: { roleList: string[] };
-  project_required_stacks: { stackList: string[] };
+  project_required_stacks: { stackList: string[] } | null;
   project_goal: string;
   project_introduction: string;
-  project_img: string;
+  project_img: string | null;
   project_views: number;
   project_created_at: string;
 }
