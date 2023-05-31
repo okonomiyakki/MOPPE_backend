b9f66f2769223b5f51a11961b329f802aac17e47
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-01 02:28:12 +0900

    Fix: jwt 인증 미들웨어 유효성 검사 로직 추가 (완료)

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -2,17 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 import { AppError } from '../utils/errorHandler';
 import { PayloadInfo } from '../database/types/UserType';
 import { AuthRequest } from '../database/types/RequestType';
-import jwt from 'jsonwebtoken';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
 import env from '../config/envconfig';
 
 const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFunction) => {
   try {
-    // const authorization = req.headers['authorization'];
     const authorization = req.cookies.Authorization;
 
     const accessToken = authorization && authorization.split(' ')[1]; // 유저 엑세스 토큰
 
-    if (!accessToken) throw new AppError(403, '[ 접근 불가 ] AccessToken을 제시해 주세요.');
+    if (!accessToken || accessToken === undefined)
+      throw new AppError(401, 'AccessToken을 제시해 주세요.');
 
     const accessTokenSecret = env.ACCESS_TOKEN_SECRET || 'MOGAKPPO_ACCESS_TOKEN_SECRET';
 
@@ -20,13 +20,15 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
 
     req.user = decodedAccessToken;
 
+    /* 엑세스 토큰 유효 시 통과 */
     next();
   } catch (accessTokenError: any) {
-    /* 엑세스 토큰 만료 */
+    // 엑세스 토큰 만료
     if (accessTokenError.name === 'TokenExpiredError') {
       const refreshToken = req.cookies.RefreshToken; // 유저 리프레시 토큰
 
-      if (!refreshToken) throw new AppError(403, '[ 접근 불가 ] RefreshToken을 제시해 주세요.');
+      if (!refreshToken || refreshToken === undefined)
+        throw new AppError(401, 'RefreshToken을 제시해 주세요.');
 
       try {
         const accessTokenSecret = env.ACCESS_TOKEN_SECRET || 'MOGAKPPO_ACCESS_TOKEN_SECRET';
@@ -39,23 +41,47 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
           expiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
         });
 
-        res.setHeader('Authorization', `Bearer ${accessToken}`); // 엑세스 토큰 재발급
+        res.setHeader('Authorization', `Bearer ${accessToken}`);
 
+        /* 엑세스 토큰 재발급 후 통과 */
         next();
       } catch (RefreshTokenError: any) {
+        // 리프레시 토큰 만료
         if (RefreshTokenError.name === 'TokenExpiredError') {
-          res.clearCookie('refreshToken'); // refresh token 이 만료된 경우
-
-          throw new AppError(
-            401,
-            '[ 접근 불가 ] RefreshToken이 만료되었습니다. 로그인을 다시 해주세요.'
-          );
+          res.clearCookie('refreshToken');
+          next(new AppError(401, 'RefreshToken이 만료되었습니다. 다시 로그인해 주세요.'));
         }
-        throw new AppError(
-          403,
-          '[ 접근 불가 ] RefreshToken이 유효하지 않습니다. 토큰을 확인해주세요.'
-        );
+        // 리프레시 토큰 유효성 문제
+        else if (RefreshTokenError.name === 'JsonWebTokenError') {
+          console.log(RefreshTokenError);
+          next(new AppError(401, 'RefreshToken이 유효하지 않습니다. 토큰을 확인해 주세요.'));
+        }
+        // 리프레시 토큰 없는 경우
+        else if (RefreshTokenError instanceof AppError) {
+          if (RefreshTokenError.statusCode === 401) console.log(RefreshTokenError);
+          next(RefreshTokenError);
+        }
+        // 서버 에러
+        else {
+          console.log(accessTokenError);
+          next(new AppError(500, '[ 접근 불가 ] 사용자 인증 실패'));
+        }
       }
+    }
+    // 엑세스 토큰 유효성 문제
+    else if (accessTokenError.name === 'JsonWebTokenError') {
+      console.log(accessTokenError);
+      next(new AppError(401, 'AccessToken이 유효하지 않습니다. 토큰을 확인해 주세요.'));
+    }
+    // 엑세스 토큰 없는 경우
+    else if (accessTokenError instanceof AppError) {
+      if (accessTokenError.statusCode === 401) console.log(accessTokenError);
+      next(accessTokenError);
+    }
+    // 서버 에러
+    else {
+      console.log(accessTokenError);
+      next(new AppError(500, '[ 접근 불가 ] 사용자 인증 실패'));
     }
   }
 };
