dc82e6a5977440ecdf7cc40a95a2e3c09e1bc793
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-05-31 21:50:29 +0900

    Feat: 프로젝트 모집글 등록 타입 생성

--- src/database/types/ProjectType.ts
+++ src/database/types/ProjectType.ts
@@ -8,8 +8,23 @@ interface ProjectProfile {
   project_recruitment_role: { roleList: string[] };
   project_required_stacks: { stackList: string[] } | null;
   project_goal: string;
+  project_participation_time: string;
   project_introduction: string;
   project_img: string | null;
   project_views: number;
   project_created_at: string;
 }
+
+export type CreateProjectInput = Pick<
+  ProjectProfile,
+  | 'user_id'
+  | 'project_type'
+  | 'project_title'
+  | 'project_summary'
+  | 'project_recruitment_role'
+  | 'project_required_stacks'
+  | 'project_goal'
+  | 'project_participation_time'
+  | 'project_introduction'
+  | 'project_img'
+>;
