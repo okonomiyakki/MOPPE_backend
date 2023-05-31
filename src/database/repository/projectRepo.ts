37d21b9e44c9b02c982342b65664fc2fe308775b
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-01 07:00:26 +0900

    Feat: 모집 역할별 모집글 목록 조회 기능 추가

--- src/database/repository/projectRepo.ts
+++ src/database/repository/projectRepo.ts
@@ -5,7 +5,7 @@ import { CreateProjectInput } from '../types/ProjectType';
 /* 모집글 등록 */
 export const createProject = async (inputData: CreateProjectInput) => {
   try {
-    const createColums = `
+    const createColumns = `
       user_id,
       project_type,
       project_title, 
@@ -28,7 +28,7 @@ export const createProject = async (inputData: CreateProjectInput) => {
 
     const SQL = `
     INSERT INTO
-    project (${createColums}) 
+    project (${createColumns}) 
     VALUES (${createValues})
     `;
 
@@ -42,3 +42,56 @@ export const createProject = async (inputData: CreateProjectInput) => {
     throw new AppError(500, '[ DB 에러 ] 모집글 등록 실패');
   }
 };
+
+/* 전체 모집글 목록 조회 */
+
+/* 모집 역할별 모집글 목록 조회 */
+export const findProjectByRole = async (project_role: string) => {
+  try {
+    const selectColumns = `
+    project_id,
+    project_type,
+    project_recruitment_status,
+    project_title,
+    project_summary,
+    project_recruitment_roles,
+    project_required_stacks,
+    project_goal,
+    project_participation_time,
+    project_views_count,
+    project_created_at
+    `;
+
+    // project_bookmark_count,
+    // project_comments_count,
+
+    const SQL = `
+    SELECT ${selectColumns}
+    FROM project
+    WHERE JSON_CONTAINS(project_recruitment_roles->'$.roleList', ?)
+    `;
+
+    const [projects] = await db.query(SQL, [project_role]);
+
+    return projects;
+  } catch (error) {
+    console.log(error);
+    throw new AppError(500, '[ DB 에러 ] 모집 역할별 모집글 목록 실패');
+  }
+};
+
+// {
+// "project_id": 1,
+// "project_type": "사이드 프로젝트",
+// "project_recruitment_status": "모집 중",
+// "project_title": "Web Development project Group",
+// "project_summary": "Join us to learn and collaborate on web development projects.",
+// "project_recruitment_roles": { "roleList": ["프론트엔드", "백엔드"] },
+// "project_required_stacks": { "stackList": ["HTML", "CSS", "JavaScript"] },
+// "project_goal": "포트폴리오/직무 역량 강화",
+// "project_participation_time": "매주 4시간 이하",
+// "project_bookmark_count": 0,
+// "project_comments_count": 0,
+// "project_views_count": 0,
+// "project_created_at": "2023-05-01 15:21:41"
+// },
