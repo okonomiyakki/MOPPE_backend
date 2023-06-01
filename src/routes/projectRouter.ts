ab0210c5dcdb66065187b5e87b5d0efb3cf69fe5
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-01 15:57:34 +0900

    Comment: 주석 수정

--- src/routes/projectRouter.ts
+++ src/routes/projectRouter.ts
@@ -7,7 +7,7 @@ const projectRouter = Router();
 /* 모집글 등록 */
 projectRouter.post('/recruitment', AuthenticateHandler, addProjectHandler);
 
-/* 모집 역할별 모집글 목록 조회 */
+/* 역할별 모집글 목록 조회 */
 projectRouter.get('/role/:project_role', getProjectsByRoleHandler);
 
 export default projectRouter;
