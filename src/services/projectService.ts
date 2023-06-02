627b718be896f8a836fa8ea09d162927f15d8141
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-02 20:47:52 +0900

    Feat: 특정 모집글의 유저 북마킹여부 필드 추가 기능 작성

--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -81,3 +81,30 @@ export const getProjectsByRole = async (user_id: number, project_role: string): 
     }
   }
 };
+
+/* 모집글 상세 정보 조회 */
+export const getProjectById = async (user_id: number, project_id: number): Promise<any> => {
+  try {
+    const foundProject = await projectRepo.findProjectById(project_id);
+
+    const foundBookmarkedProjects = await bookmarkRepo.findBookmarkedProjectsById(user_id);
+
+    // 게시글이 존재하는지 확인 후 없으면 에러 처리
+
+    const BookmarkedProjectIds = foundBookmarkedProjects.map((project) => project.project_id);
+
+    const projectInfo = BookmarkedProjectIds.includes(project_id)
+      ? { ...foundProject, is_bookmarked: true }
+      : { ...foundProject, is_bookmarked: false };
+
+    return projectInfo;
+  } catch (error) {
+    if (error instanceof AppError) {
+      if (error.statusCode === 500) console.log(error);
+      throw error;
+    } else {
+      console.log(error);
+      throw new AppError(500, '[ 서버 에러 ] 모집글 상세 정보 조회 실패');
+    }
+  }
+};
