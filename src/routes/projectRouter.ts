609184bf4a6070cc02b2bea7ba3198b21af4e892
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-02 17:02:02 +0900

    Feat: 전체 모집글 목록 조회 라우터 생성

--- src/routes/projectRouter.ts
+++ src/routes/projectRouter.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import { addProjectHandler, getProjectsByRoleHandler } from '../controllers/projectController';
+import {
+  addProjectHandler,
+  getProjectsByRoleHandler,
+  getAllProjectsHandler,
+} from '../controllers/projectController';
 import AuthenticateHandler from '../middlewares/jwt';
 
 const projectRouter = Router();
 
 /* 모집글 등록 */
 projectRouter.post('/recruitment', AuthenticateHandler, addProjectHandler);
+
+/* 전체 모집글 목록 조회 */
+projectRouter.get('/', AuthenticateHandler, getAllProjectsHandler);
 
 /* 역할별 모집글 목록 조회 */
 projectRouter.get('/role/:project_role', AuthenticateHandler, getProjectsByRoleHandler);
