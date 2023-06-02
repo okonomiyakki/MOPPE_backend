b1c7c75fd8de35c2db1c79a5ef798632afc9b260
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-02 16:58:57 +0900

    Feat: 전체 모집글 목록 조회 기능 추가

--- src/database/repository/projectRepo.ts
+++ src/database/repository/projectRepo.ts
@@ -44,6 +44,40 @@ export const createProject = async (inputData: P.CreateProjectInput): Promise<P.
 };
 
 /* 전체 모집글 목록 조회 */
+export const findAllProjects = async (): Promise<any> => {
+  try {
+    const selectColumns = `
+    project.project_id,
+    project.project_type,
+    project.project_recruitment_status,
+    project.project_title,
+    project.project_summary,
+    project.project_recruitment_roles,
+    project.project_required_stacks,
+    project.project_goal,
+    project.project_participation_time,
+    COUNT(bookmark.project_id) AS project_bookmark_count,
+    COUNT(comment.project_id) AS project_comments_count,
+    project.project_views_count,
+    project.project_created_at
+    `;
+
+    const SQL = `
+    SELECT ${selectColumns}
+    FROM project
+    LEFT JOIN bookmark ON bookmark.project_id = project.project_id
+    LEFT JOIN comment ON comment.project_id = project.project_id
+    GROUP BY project.project_id
+    `;
+
+    const [projects]: any = await db.query(SQL);
+
+    return projects;
+  } catch (error) {
+    console.log(error);
+    throw new AppError(500, '[ DB 에러 ] 전체 모집글 목록 조회 실패');
+  }
+};
 
 /* 역할별 모집글 목록 조회 */
 export const findProjectsByRole = async (project_role: string): Promise<any> => {
@@ -78,6 +112,6 @@ export const findProjectsByRole = async (project_role: string): Promise<any> => 
     return projects;
   } catch (error) {
     console.log(error);
-    throw new AppError(500, '[ DB 에러 ] 역할별 모집글 목록 실패');
+    throw new AppError(500, '[ DB 에러 ] 역할별 모집글 목록 조회 실패');
   }
 };
