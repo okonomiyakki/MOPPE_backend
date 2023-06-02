613e14221d135ae0646a3e68d2eee345586932aa
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-02 20:44:29 +0900

    Fix: 인증 미들웨어 흐름 확인용 콘솔 작성

--- src/middlewares/jwt.ts
+++ src/middlewares/jwt.ts
@@ -10,7 +10,11 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
     // const authorization = req.cookies.Authorization;
     const authHeader = req.headers['authorization'];
 
+    console.log(authHeader);
+
     const accessToken = authHeader && authHeader.split(' ')[1]; // 유저 엑세스 토큰
+
+    console.log(accessToken);
 
     /* 그냥 방문자인 경우 통과*/
     if (accessToken === undefined && req.method === 'GET') {
@@ -18,11 +22,11 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
         user_id: 0,
         user_email: 'GUEST@gmail.com',
       };
-      console.log('방문자임');
+      console.log('지금 방문자임');
       return next();
     }
 
-    console.log('회원임');
+    console.log('지금 회원임');
 
     if (!accessToken || accessToken === undefined)
       throw new AppError(401, 'AccessToken을 제시해 주세요.');
@@ -31,12 +35,15 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
 
     const decodedAccessToken = jwt.verify(accessToken, accessTokenSecret) as PayloadInfo;
 
+    console.log(decodedAccessToken);
     req.user = decodedAccessToken;
 
     /* 엑세스 토큰 유효 시 통과 */
+    console.log('엑세스 토큰 유효 시 통과');
     next();
   } catch (accessTokenError: any) {
     // 엑세스 토큰 만료
+    console.log('엑세스 토큰 만료');
     if (accessTokenError.name === 'TokenExpiredError') {
       const refreshToken = req.cookies.RefreshToken; // 유저 리프레시 토큰
 
@@ -54,13 +61,15 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
           expiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
         });
 
-        res.setHeader('Authorization', `Bearer ${accessToken}`);
+        console.log('엑세스 재발급: ', accessToken);
+        // res.setHeader('Authorization', `Bearer ${accessToken}`);
 
         /* 엑세스 토큰 재발급 후 통과 */
         next();
       } catch (RefreshTokenError: any) {
         // 리프레시 토큰 만료
-        if (RefreshTokenError.name === 'TokenExpiredError') {
+        if (RefreshTokenError.message === 'TokenExpiredError') {
+          console.log('리프레시 토큰 만료');
           res.clearCookie('refreshToken');
           next(new AppError(401, 'RefreshToken이 만료되었습니다. 다시 로그인해 주세요.'));
         }
@@ -76,7 +85,7 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
         }
         // 서버 에러
         else {
-          console.log(accessTokenError);
+          console.log(RefreshTokenError);
           next(new AppError(500, '[ 접근 불가 ] 사용자 인증 실패'));
         }
       }
