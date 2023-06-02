5d96dbbf6a80e61b98b59531deb35ce2571fc5e1
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-02 14:07:52 +0900

    Feat: API version 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,8 +5,8 @@ import projectRouter from './projectRouter';
 
 const router = Router();
 
-router.use('/', rootRouter);
-router.use('/users', userRouter);
-router.use('/projects', projectRouter);
+router.use('/v1', rootRouter);
+router.use('/v1/users', userRouter);
+router.use('/v1/projects', projectRouter);
 
 export default router;
