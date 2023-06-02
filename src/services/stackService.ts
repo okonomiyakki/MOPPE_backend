e2cdf10e82c7a5f42e96d40a49d671612d9ed111
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-02 21:10:14 +0900

    Feat: stack_name value만 갖는 리스트로 재할당

---
+++ src/services/stackService.ts
@@ -0,0 +1,21 @@
+import { AppError } from '../utils/errorHandler';
+import * as stackRepo from '../database/repository/stackRepo';
+
+/* 전체 기술 스택 리스트 조회 */
+export const getAllStacks = async (): Promise<any> => {
+  try {
+    const foundProject = await stackRepo.findAllStacks();
+
+    const stacks = foundProject.map((stack) => stack.stack_name);
+
+    return stacks;
+  } catch (error) {
+    if (error instanceof AppError) {
+      if (error.statusCode === 500) console.log(error);
+      throw error;
+    } else {
+      console.log(error);
+      throw new AppError(500, '[ 서버 에러 ] 전체 기술 스택 리스트 조회 실패');
+    }
+  }
+};
