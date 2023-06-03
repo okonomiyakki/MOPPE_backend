54881ed9a9b1cab532ddb8e569dae555795ea1b3
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-04 01:40:20 +0900

    Feat: 엑세스 토큰 재발급 함수 작성

---
+++ src/utils/AccessTokenGenerator.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import env from '../config/envconfig';
+import * as U from '../types/UserType';
+
+export const generateNewAccessToken = (decodedRefreshToken: U.decodedToken) => {
+  const newPayload: U.PayloadInfo = {
+    user_id: decodedRefreshToken.user_id,
+    user_email: decodedRefreshToken.user_email,
+  };
+
+  const accessTokenSecret = env.ACCESS_TOKEN_SECRET || 'MOGAKPPO_ACCESS_TOKEN_SECRET';
+
+  const newAccessToken = jwt.sign(newPayload, accessTokenSecret, {
+    expiresIn: env.ACCESS_TOKEN_EXPIRES_IN,
+  });
+
+  const newAccessTokenInfo = {
+    newAccessToken,
+  };
+  return newAccessTokenInfo;
+};
