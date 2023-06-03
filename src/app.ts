9e79606a2f9ac7ffa963d86415339d81c8f205dc
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-04 00:26:21 +0900

    Fix: localhost cors 허용 도메인에 추가

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,9 @@ import { errorHandlerMiddleware } from './middlewares/errorHandler';
 const app = express();
 const port = Number(env.PORT || 3000);
 const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
   'http://127.0.0.1:3002',
