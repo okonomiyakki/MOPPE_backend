1cd2f1bf85e78951e715bd2d503aa6d0f69cad74
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-08 13:59:42 +0900

    Fix: 에러별 핸들링 함수 작성

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+// import AppError from '../types/AppErrorType';
 
 class AppError extends Error {
   statusCode: number;
@@ -10,7 +11,7 @@ class AppError extends Error {
   }
 }
 
-const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
   const { statusCode, message } = err;
   res.status(statusCode || 500).json({
     status: 'Error',
@@ -19,4 +20,24 @@ const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunc
   });
 };
 
-export { AppError, errorHandler };
+export const handleBadRequest = (message: string = '잘못된 요청입니다.') => {
+  throw new AppError(400, message);
+};
+
+export const handleInternalServerError = (message: string = '내부 서버 오류가 발생했습니다.') => {
+  throw new AppError(500, message);
+};
+
+export const handleNotFound = (message: string = '찾을 수 없습니다.') => {
+  throw new AppError(404, message);
+};
+
+export const handleForbidden = (message: string = '접근이 거부되었습니다.') => {
+  throw new AppError(403, message);
+};
+
+export const handleUnauthorized = (message: string = '인증되지 않았습니다.') => {
+  throw new AppError(401, message);
+};
+
+export { AppError };
