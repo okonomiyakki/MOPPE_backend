db50e20b134ed0000123407213a5a852f5e29c0e
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-08 15:58:48 +0900

    Fix: [코드리뷰] jwt 인증 미들웨어 에러처리 수정 (2차)

--- src/middlewares/authHandler.ts
+++ src/middlewares/authHandler.ts
@@ -1,6 +1,8 @@
 import { Response, NextFunction } from 'express';
 import { AuthRequest } from '../types/RequestType';
-import { AppError } from './errorHandler';
+// import { AppError } from './errorHandler';
+import AppError from '../types/AppErrorType';
+import * as AppErrors from '../middlewares/errorHandler';
 import jwt from 'jsonwebtoken';
 import env from '../config/envconfig';
 import * as U from '../types/UserType';
@@ -9,7 +11,7 @@ import { generateNewAccessToken } from '../utils/accessTokenGenerator';
 const nextForGuest = (req: AuthRequest, next: NextFunction) => {
   req.user = {
     user_id: 0,
-    user_email: 'GUEST@gmail.com',
+    user_email: 'GUEST@email.com',
   };
   return next();
 };
@@ -29,35 +31,31 @@ const generateNewAccessTokenHandler = async (
     // const refreshToken = authHeader && authHeader.split(' ')[1].split('refreshToken=')[1];
 
     if (refreshToken === undefined)
-      throw new AppError(401, 'AccessToken이 만료되었습니다. RefreshToken을 보내주세요.');
+      AppErrors.handleUnauthorized('AccessToken이 만료되었습니다. RefreshToken을 보내주세요.');
 
     const refreshTokenSecret = env.REFRESH_TOKEN_SECRET || 'MOGAKPPO_REFRESH_TOKEN_SECRET';
 
     /* RefreshToken 검증 */
-    const decodedRefreshToken = jwt.verify(refreshToken, refreshTokenSecret) as U.decodedToken;
+    if (refreshToken) {
+      const decodedRefreshToken = jwt.verify(refreshToken, refreshTokenSecret) as U.decodedToken;
 
-    const currentTime = Math.floor(Date.now() / 1000);
+      const currentTime = Math.floor(Date.now() / 1000);
 
-    /* RefreshToken 검증 완료 시 만료시간 검사*/
-    if (decodedRefreshToken.exp >= currentTime) {
-      const newAccessTokenInfo = generateNewAccessToken(decodedRefreshToken);
+      /* RefreshToken 검증 완료 시 만료시간 검사*/
+      if (decodedRefreshToken.exp >= currentTime) {
+        const newAccessTokenInfo = generateNewAccessToken(decodedRefreshToken);
 
-      /* 응답 바디로 AccessToken 재발급 */
-      res.status(200).json({ message: 'generate new AccessToken', data: newAccessTokenInfo });
+        /* 응답 바디로 AccessToken 재발급 */
+        res.status(200).json({ message: 'generate new AccessToken', data: newAccessTokenInfo });
+      }
     }
   } catch (error: any) {
-    if (error instanceof AppError) {
-      if (error.statusCode === 401) console.log(error);
-      next(error);
-    } else if (error.message === 'jwt expired') {
-      next(new AppError(401, '[ 토큰 만료 ] RefreshToken이 만료되었습니다. 다시 로그인해 주세요.'));
-    } else if (error.message === 'jwt malformed') {
-      console.log(error);
-      next(new AppError(401, '[ 검증 실패 ] RefreshToken이 유효하지 않습니다.'));
-    } else {
-      console.log(error);
-      next(new AppError(401, '[ 서버 에러 ] 사용자 인증 실패'));
-    }
+    if (error instanceof AppError) next(error);
+    else if (error.message === 'jwt expired')
+      next(AppErrors.handleUnauthorized('RefreshToken이 만료되었습니다. 다시 로그인해 주세요.'));
+    else if (error.message === 'jwt malformed')
+      next(AppErrors.handleUnauthorized('RefreshToken이 유효하지 않습니다.'));
+    else next(AppErrors.handleInternalServerError());
   }
 };
 
@@ -73,36 +71,31 @@ const AuthenticateHandler = async (req: AuthRequest, res: Response, next: NextFu
     if (accessToken === undefined && req.method === 'GET') return nextForGuest(req, next);
 
     if (accessToken === undefined)
-      throw new AppError(
-        401,
-        'AccessToken이 존재하지 않습니다. 회원가입 및 로그인 후 이용해 주세요.'
-      ); // TODO] 에러코드 수정
+      AppErrors.handleUnauthorized('AccessToken이 존재하지 않습니다. 로그인 후 이용해 주세요.');
 
     const accessTokenSecret = env.ACCESS_TOKEN_SECRET || 'MOGAKPPO_ACCESS_TOKEN_SECRET';
 
     /* AccessToken 검증 */
-    const decodedAccessToken = jwt.verify(accessToken, accessTokenSecret) as U.decodedToken;
+    if (accessToken) {
+      const decodedAccessToken = jwt.verify(accessToken, accessTokenSecret) as U.decodedToken;
 
-    const currentTime = Math.floor(Date.now() / 1000);
+      const currentTime = Math.floor(Date.now() / 1000);
 
-    /* AccessToken 검증 완료 시 만료시간 검사 */
-    if (decodedAccessToken.exp >= currentTime) {
-      req.user = decodedAccessToken;
+      /* AccessToken 검증 완료 시 만료시간 검사 */
+      if (decodedAccessToken.exp >= currentTime) {
+        req.user = decodedAccessToken;
 
-      /* AccessToken이 만료되지 않았으면 통과 */
-      return next();
+        /* AccessToken이 만료되지 않았으면 통과 */
+        return next();
+      }
     }
   } catch (error: any) {
-    if (error instanceof AppError) {
-      if (error.statusCode === 401) console.log(error);
-      next(error);
-    } else if (error.message === 'jwt expired') {
+    if (error instanceof AppError) next(error);
+    else if (error.message === 'jwt expired')
       /* AccessToken 만료 시 재발급 함수 호출 */
       generateNewAccessTokenHandler(req, res, next);
-    } else if (error.message === 'jwt malformed') {
-      console.log(error);
-      next(new AppError(401, '[ 검증 실패 ] AccessToken이 유효하지 않습니다.'));
-    }
+    else if (error.message === 'jwt malformed')
+      next(AppErrors.handleUnauthorized('AccessToken이 유효하지 않습니다.'));
   }
 };
 
