11a484a88689e6cbe0219693def3ed05e424c23f
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-08 17:30:38 +0900

    Fix: 에러 핸들러 수정

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,43 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
-// import AppError from '../types/AppErrorType';
+import AppError from '../types/AppErrorType';
 
-class AppError extends Error {
-  statusCode: number;
-  message: string;
-  constructor(statusCode: number, message: string) {
-    super();
-    this.statusCode = statusCode;
-    this.message = message;
-  }
-}
+export const handleBadRequest = (message: string = 'Bad Request'): AppError => {
+  throw new AppError(400, message);
+};
+
+export const handleUnauthorized = (message: string = 'Unauthorized'): AppError => {
+  throw new AppError(401, message);
+};
+
+export const handleForbidden = (message: string = 'Forbidden'): AppError => {
+  throw new AppError(403, message);
+};
+
+export const handleNotFound = (message: string = 'Not Found'): AppError => {
+  throw new AppError(404, message);
+};
+
+export const handleInternalServerError = (message: string = 'Internal Server Error'): AppError => {
+  throw new AppError(500, message);
+};
 
 export const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
   const { statusCode, message } = err;
+  console.log(err);
   res.status(statusCode || 500).json({
     status: 'Error',
     statusCode: statusCode || 500,
     message: message || '서버에서 에러가 발생했습니다.',
   });
 };
-
-export const handleBadRequest = (message: string = '잘못된 요청입니다.') => {
-  throw new AppError(400, message);
-};
-
-export const handleInternalServerError = (message: string = '내부 서버 오류가 발생했습니다.') => {
-  throw new AppError(500, message);
-};
-
-export const handleNotFound = (message: string = '찾을 수 없습니다.') => {
-  throw new AppError(404, message);
-};
-
-export const handleForbidden = (message: string = '접근이 거부되었습니다.') => {
-  throw new AppError(403, message);
-};
-
-export const handleUnauthorized = (message: string = '인증되지 않았습니다.') => {
-  throw new AppError(401, message);
-};
-
-export { AppError };
