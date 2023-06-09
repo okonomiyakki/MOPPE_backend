4117b0bd72fe2d395ac95764055f2710e388d23e
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-09 16:58:55 +0900

    Feat: 포트폴리오 상세 정보 조회 기능 수정 중 (1차)

--- src/database/repository/portfolioRepo.ts
+++ src/database/repository/portfolioRepo.ts
@@ -33,3 +33,46 @@ export const createPorfolio = async (inputData: Portfolio.CreateInput): Promise<
     throw error;
   }
 };
+
+/* 포트폴리오 상세 정보 조회 */
+export const findPortfolioById = async (portfolio_id: number): Promise<any> => {
+  try {
+    const selectColumns = `
+    portfolio.portfolio_id,
+    portfolio.portfolio_title,
+    portfolio.portfolio_summary,
+    portfolio.portfolio_thumbnail,
+    portfolio.portfolio_github,
+    portfolio.portfolio_stacks,
+    portfolio.portfolio_description,
+    portfolio.portfolio_img,
+    COUNT(DISTINCT bookmark.user_id) AS portfolio_bookmark_count,
+    COUNT(DISTINCT comment.comment_id) AS portfolio_comments_count,
+    portfolio.portfolio_views_count,
+    portfolio.portfolio_created_at,
+    user.user_id,
+    user.user_name,
+    user.user_introduction,
+    user.user_img
+    `;
+
+    const SQL = `
+    SELECT ${selectColumns}
+    FROM portfolio
+    LEFT JOIN bookmark ON bookmark.portfolio_id = portfolio.portfolio_id
+    LEFT JOIN comment ON comment.portfolio_id = portfolio.portfolio_id
+    JOIN user ON user.user_id = portfolio.user_id
+    WHERE portfolio.portfolio_id = ?
+    `;
+
+    const [portfolio]: any = await db.query(SQL, [portfolio_id]);
+
+    const isPortfolioValid = portfolio[0];
+
+    if (!isPortfolioValid) AppErrors.handleNotFound('이미 삭제된 포트폴리오 입니다.');
+
+    return portfolio[0];
+  } catch (error) {
+    throw error;
+  }
+};
--- src/services/portfolioService.ts
+++ src/services/portfolioService.ts
@@ -1,5 +1,6 @@
 import * as portfolioRepo from '../database/repository/portfolioRepo';
 import * as Portfolio from '../types/PortfolioType';
+import { generateNewDate } from '../utils/dateGenerator';
 
 /* 포트폴리오 등록 */
 export const addPorfolio = async (inputData: Portfolio.CreateInput): Promise<Portfolio.Id> => {
@@ -11,3 +12,37 @@ export const addPorfolio = async (inputData: Portfolio.CreateInput): Promise<Por
     throw error;
   }
 };
+
+// /* 포트폴리오 상세 정보 조회 */
+// // TODO] 라우터 변경 한 다음 추가하기
+// export const getPortfolioById = async (user_id: number, portfolio_id: number): Promise<any> => {
+//   try {
+//     const foundPortfolio = await portfolioRepo.findPortfolioById(portfolio_id);
+
+//     // const foundBookmarkedUsers = await bookmarkRepo.findBookmarkedUsersById(portfolio_id);
+
+//     // const foundBookmarkedProjects = await bookmarkRepo.findBookmarkedProjectsById(user_id);
+
+//     const currentKorDate = generateNewDate();
+
+//     const isUserEnteredCurrentDate = await portfolioRepo.findUserViewDateById(
+//       user_id,
+//       portfolio_id,
+//       currentKorDate
+//     );
+
+//     // if (!isUserEnteredCurrentDate) {
+//     //   await portfolioRepo.updateProjectViewsCount(user_id, portfolio_id, currentKorDate);
+//     // }
+
+//     // const bookmarkedProjectIds = foundBookmarkedProjects.map((project) => project.portfolio_id);
+
+//     const checkIsBookmarked = bookmarkedProjectIds.includes(portfolio_id)
+//       ? { ...foundProject, project_bookmark_users: foundBookmarkedUsers, is_bookmarked: true }
+//       : { ...foundProject, project_bookmark_users: foundBookmarkedUsers, is_bookmarked: false };
+
+//     return checkIsBookmarked;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
