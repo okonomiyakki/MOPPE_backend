647cdc25855ae83483df46405d6bc4bab82534f1
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-11 05:41:39 +0900

    Feat: 유저 관련 DTO 파일 생성 및 회원가입 DTO 구현

---
+++ src/database/dtos/userDto.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+
+export class SignUpDto {
+  @IsNotEmpty({ message: '이름이 입력되지 않았습니다.' })
+  @IsString({ message: '이름이 문자열 형식이 아닙니다.' })
+  public user_name: string;
+
+  @IsNotEmpty({ message: '이메일이 입력되지 않았습니다.' })
+  @IsString({ message: '이메일이 문자열 형식이 아닙니다.' })
+  @IsEmail({}, { message: '올바른 이메일 형식이 아닙니다.' })
+  public user_email: string;
+
+  @IsNotEmpty({ message: '비밀번호가 입력되지 않았습니다.' })
+  @IsString({ message: '비밀번호가 문자열 형식이 아닙니다.' })
+  public user_password: string;
+
+  constructor(user_email: string, user_name: string, user_password: string) {
+    this.user_email = user_email;
+    this.user_name = user_name;
+    this.user_password = user_password;
+  }
+}
