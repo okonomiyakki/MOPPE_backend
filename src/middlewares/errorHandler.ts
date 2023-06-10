1e2535f4bde6850fef48708aa4244964c0e54447
[SW4/2반] 박지원 <parkji12532@naver.com>
2023-06-11 05:39:29 +0900

    Fix: 에러행들러 전파 오류 수정

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,28 +2,30 @@ import { Request, Response, NextFunction } from 'express';
 import AppError from '../types/AppErrorType';
 
 export const handleBadRequest = (message: string = 'Bad Request'): AppError => {
-  throw new AppError(400, message);
+  return new AppError(400, message);
 };
 
 export const handleUnauthorized = (message: string = 'Unauthorized'): AppError => {
-  throw new AppError(401, message);
+  return new AppError(401, message);
 };
 
 export const handleForbidden = (message: string = 'Forbidden'): AppError => {
-  throw new AppError(403, message);
+  return new AppError(403, message);
 };
 
 export const handleNotFound = (message: string = 'Not Found'): AppError => {
-  throw new AppError(404, message);
+  return new AppError(404, message);
 };
 
 export const handleInternalServerError = (message: string = 'Internal Server Error'): AppError => {
-  throw new AppError(500, message);
+  return new AppError(500, message);
 };
 
 export const errorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
   const { statusCode, message } = err;
+
   console.log(err);
+
   res.status(statusCode || 500).json({
     status: 'Error',
     statusCode: statusCode || 500,
