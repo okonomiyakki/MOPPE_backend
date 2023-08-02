99c4e8b81db42fd199cfdbbafe0f007afb7b289b
okonomiyakki <parkji12532@naver.com>
2023-08-02 22:03:07 +0900

    Docs: README.md 수정

--- src/app.ts
+++ src/app.ts
@@ -8,34 +8,7 @@ import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 const port = Number(env.PORT || 3000);
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:3001',
-  'http://localhost:3002',
-  'http://127.0.0.1:3000',
-  'http://127.0.0.1:3001',
-  'http://127.0.0.1:3002',
-  'http://34.64.242.119',
-  'http://34.64.242.119:80',
-  'https://34.64.242.119',
-  'https://34.64.242.119:80',
-  'https://34.64.242.119:3000',
-  'http://kdt-sw-4-team07.elicecoding.com',
-  'http://kdt-sw-4-team07.elicecoding.com:80',
-  'http://kdt-sw-4-team07.elicecoding.com:3000',
-  'http://kdt-sw-4-team07.elicecoding.com:5000',
-  'http://192.168.200.104:3000',
-  'http://192.168.0.15:3000',
-  'http://192.168.123.141:3000',
-  'http://192.168.0.5:3000',
-  'http://172.30.1.42:3000',
-  'http://192.168.0.64:3000',
-  'http://192.168.0.*:3000',
-  'http://13.209.192.1',
-  'https://13.209.192.1',
-  'http://moppe.co.kr',
-  'https://moppe.co.kr',
-];
+const allowedOrigins = ['http://localhost:3000', 'https://moppe.co.kr'];
 
 const corsOptions = {
   origin: allowedOrigins,
--- src/routes/rootRouter.ts
+++ src/routes/rootRouter.ts
@@ -4,7 +4,7 @@ const rootRouter = Router();
 
 rootRouter.get('/', (req: Request, res: Response) => {
   res.send(
-    `<h1 style="width: 100%; background: lightgray; margin: 0; padding: 0; text-align: center;">모각뽀 루트 페이지입니다 ㅎㅎ</h1>`
+    `<h1 style="width: 100%; background: lightgray; margin: 0; padding: 0; text-align: center;">모프 루트 페이지입니다 ㅎㅎ</h1>`
   );
 });
 
